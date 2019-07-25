5a8cf5f8bd793c3c0905c88a2b65e2d73969165a
Ubuntu <coen@WeatherStationAPI.3danpcsfdtsebfplg3gp23x0fe.zx.internal.cloudapp.net>
2019-07-25 08:17:47 +0000

    Fix repo structure

---
+++ ServerScripts/weatherstationapi.sh
@@ -0,0 +1,44 @@
+#! /bin/sh
+#
+### BEGIN INIT INFO
+# Provides:          weatherstationapi.sh
+# Required-Start:    $remote_fs $local_fs
+# Required-Stop:     $remote_fs $local_fs
+# Should-Start:
+# Should-Stop:
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: AnchormenStations API
+# Description:
+### END INIT INFO
+
+DAEMON=/usr/local/bin/noip2
+NAME=weatherstationapi
+
+test -x $DAEMON || exit 0
+
+case "$1" in
+    start)
+    echo -n "Starting AnchormenStations API: "
+    start-stop-daemon --start --exec $DAEMON
+    echo "weatherstationapi."
+    ;;
+    stop)
+    echo -n "Shutting AnchormenStations API:"
+    start-stop-daemon --stop --oknodo --retry 30 --exec $DAEMON
+    echo "weatherstationapi."
+    ;;
+
+    restart)
+    echo -n "Restarting AnchormenStations API: "
+    start-stop-daemon --stop --oknodo --retry 30 --exec $DAEMON
+    start-stop-daemon --start --exec $DAEMON
+    echo "weatherstationapi."
+    ;;
+
+    *)
+    echo "Usage: $0 {start|stop|restart}"
+    exit 1
+esac
+exit 0
+
