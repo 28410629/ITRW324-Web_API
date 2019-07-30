5ca4f86f85410fe309a5074f621f4b7e0dd10d10
Coenraad Human <coen.human@gmail.com>
2019-07-30 13:55:07 +0200

    Made final adjustments to utility.

--- WeatherStationApi/01 Common/Utilities/LogErrorEmail.cs
+++ WeatherStationApi/01 Common/Utilities/LogErrorEmail.cs
@@ -7,19 +7,16 @@ namespace WeatherStationApi._01_Common.Utilities
 {
     public class LogErrorEmail
     {
-        private static string source;
-        private static string message;
-        private static string stackTrace;
-        private static readonly string username = "itrw324.weatherstationapi@gmail.com";
-        /*private static readonly string username = "itrw324.weatherstationapi@outlook.com";
-        private static readonly string password = "VUarH4vsNk6DTCzy";
-        private static readonly string host = "smtp-mail.outlook.com"; */
+        private static string _source;
+        private static string _message;
+        private static string _stackTrace;
 
         public static void SendError(Exception e)
         {
-            message = e.Message;
-            source = e.Source;
-            stackTrace = e.StackTrace;
+            Console.WriteLine("[ OK  ] LogErrorEmail: Sending exception occured email.");
+            _message = e.Message;
+            _source = e.Source;
+            _stackTrace = e.StackTrace;
             Thread thread = new Thread(new ThreadStart(SendError));
             thread.Start();
         }
@@ -27,9 +24,9 @@ namespace WeatherStationApi._01_Common.Utilities
         public static void SendErrorTest()
         {
             Console.WriteLine("[ OK  ] LogErrorEmail: Sending test email.");
-            message = "This is a test!";
-            source = "This is a test!";
-            stackTrace = "This is a test!";
+            _message = "This is the exception's message.";
+            _source = "This is the exception's source.";
+            _stackTrace = "This is the exception's stacktrace.";
             Thread thread = new Thread(new ThreadStart(SendError));
             thread.Start();
         }
@@ -53,8 +50,15 @@ namespace WeatherStationApi._01_Common.Utilities
                         mailMessage.To.Insert(1, new MailAddress("morneventer.mv@gmail.com"));
                         mailMessage.To.Insert(2, new MailAddress("eonviljoen.ev@gmail.com"));
                         mailMessage.Subject = "WeatherStationAPI - System Error:  " + DateTime.Today.ToLongDateString();
-                        mailMessage.Body = DateTime.Now.ToString("h:mm:ss tt") + "\n\n" + message + "\n\n" + source +
-                                           "\n\n" + message + "\n\n" + stackTrace +
+                        mailMessage.Body = DateTime.Now.ToString("h:mm:ss tt") + 
+                                           "\n\n" +
+                                           _message +
+                                           "\n\n" +
+                                           _source +
+                                           "\n\n" +
+                                           _message +
+                                           "\n\n" +
+                                           _stackTrace +
                                            "\n\n-------------------------------------------FIN-------------------------------------------";
 
                         //send email
@@ -65,7 +69,7 @@ namespace WeatherStationApi._01_Common.Utilities
             }
             catch (Exception e)
             {
-                Console.WriteLine("[ ERR ] LogErrorEmail: " + e.Message + "\n\n" + e.StackTrace);
+                Console.WriteLine("[ ERR ] LogErrorEmail: " + e.Message);
             }
         }
     }
