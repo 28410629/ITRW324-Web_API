a7b3f6285c3e282a39aec784e06da6f7cd5252a7
EonViljoen <eonviljoen.ev@gmail.com>
2019-08-05 19:19:19 +0200

    Initial averageweek service implemented
    Not working yet though

--- WeatherStationApi/06 Services/AverageWeekReadingsService.cs
+++ WeatherStationApi/06 Services/AverageWeekReadingsService.cs
@@ -1,7 +1,63 @@
+using System;
+using System.Linq;
+using WeatherStationApi._03_Dtos;
+using WeatherStationApi._04_Interfaces.Repositories;
+using WeatherStationApi._05_Repositories;
+
 namespace WeatherStationApi._06_Services
 {
     public class AverageWeekReadingsService
     {
+        private static readonly DataContextFactory _factory = new DataContextFactory();
+        private readonly IReadingsRepository _readingsRepository = new ReadingsRepository(_factory);
+
+        public ReadingsDto AverageWeekReadings()
+        {
+            var readings = _readingsRepository
+                .FetchAll()
+                .Where(x => x.ReadingDateTime <= DateTime.Now.AddDays(31))
+                .Select(x => new ReadingDto(x).Temperature.Average(x));
+            
+            return new ReadingsDto()
+            {
+                Readings = readings.ToList();
+            };
+
+        }
         
+        /*private static readonly DataContextFactory _factory = new DataContextFactory();
+        private readonly IReadingsRepository _readingsRepository = new ReadingsRepository(_factory);
+
+        public ReadingsDto FetchReadings()
+        {
+            var readings =  _readingsRepository
+                .FetchAll()
+                .Select(x => new ReadingDto(x));
+
+            return new ReadingsDto
+            {
+                Readings = readings.ToList()
+            };
+        }
+        
+        
+
+        private static readonly DataContextFactory _factory = new DataContextFactory();
+        private readonly IReadingsRepository _readingsRepository = new ReadingsRepository(_factory);
+
+        public ReadingsDto FetchLastDayReadings()
+        {
+            var readings =  _readingsRepository
+                .FetchAll()
+                .Where(x => x.ReadingDateTime >= DateTime.Now.AddDays(-1))
+                .Select(x => new ReadingDto(x));
+
+            
+            return new ReadingsDto
+            {
+                Readings = readings.ToList()
+            };
+        }*/
+    
     }
 }
