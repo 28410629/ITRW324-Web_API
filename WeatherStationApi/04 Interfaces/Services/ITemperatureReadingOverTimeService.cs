5d358b1d89ccb2aa19566525381bbbed5e04f312
Eon Viljoen <eonviljoen.ev@gmail.com>
2019-09-04 17:25:20 +0200

    New Dtos

--- WeatherStationApi/04 Interfaces/Services/ITemperatureReadingOverTimeService.cs
+++ WeatherStationApi/04 Interfaces/Services/ITemperatureReadingOverTimeService.cs
@@ -4,6 +4,11 @@ namespace WeatherStationApi._04_Interfaces.Services
 {
     public interface ITemperatureReadingOverTimeService
     {
+        TemperatureReadingsOverTimeDto FetchAllStationDayTemperatureReadingsOverTime();
+
+        TemperatureReadingsOverTimeDto FetchAllStationWeekTemperatureReadingsOverTime();
+        
+        TemperatureReadingsOverTimeDto FetchAllStationMonthTemperatureReadingsOverTime();
         TemperatureReadingsOverTimeDto FetchStationDayTemperatureReadingsOverTime(int StationId);
         
         TemperatureReadingsOverTimeDto FetchStationWeekTemperatureReadingsOverTime(int StationId);
