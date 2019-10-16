b4241c83a0d6eccbc73603e432d86a34d1bb69a0
Eon VIljoen <28807995@student.g.nwu.ac.za>
2019-10-16 19:35:30 +0200

    Last services interfaces

--- WeatherStationApi/04 Interfaces/Services/IStationListService.cs
+++ WeatherStationApi/04 Interfaces/Services/IStationListService.cs
@@ -2,6 +2,7 @@ using WeatherStationApi._03_Dtos;
 
 namespace WeatherStationApi._04_Interfaces.Services
 {
+    // explicitly states methods for StationListsDto.
     public interface IStationListService
     {
         StationListsDto FetchStations();
--- WeatherStationApi/04 Interfaces/Services/IStationSaveReadingService.cs
+++ WeatherStationApi/04 Interfaces/Services/IStationSaveReadingService.cs
@@ -2,6 +2,7 @@ using WeatherStationApi._03_Dtos;
 
 namespace WeatherStationApi._04_Interfaces
 {
+    // explicitly states methods for ReadingDto.
     public interface IStationSaveReadingService
     {
         void CreateNewReading(ReadingDto reading);
