d3959b3db3356eb4b12f1c402e47403b4163afa5
Eon VIljoen <28807995@student.g.nwu.ac.za>
2019-10-16 19:24:59 +0200

    Pascalcase and comments on location and raw readings interface

--- WeatherStationApi/04 Interfaces/Services/ILocationReadingsService.cs
+++ WeatherStationApi/04 Interfaces/Services/ILocationReadingsService.cs
@@ -5,16 +5,16 @@ namespace WeatherStationApi._04_Interfaces.Services
 {
     public interface ILocationReadingsService
     {
-        // interface for accessing FetchLocationDetailDay in LocationReadingsDto.
+        // interface for accessing FetchLocationDetailDay from LocationReadingsDto.
         LocationReadingsDto FetchLocationDetailDay(string Province, string City, DateTime Date);
 
-        // interface for accessing FetchLocationDetailWeek in LocationReadingsDto.
+        // interface for accessing FetchLocationDetailWeek from LocationReadingsDto.
         LocationReadingsDto FetchLocationDetailWeek(string Province, string City, DateTime Date);
 
-        // interface for accessing FetchLocationDetailMonth in LocationReadingsDto.
+        // interface for accessing FetchLocationDetailMonth from LocationReadingsDto.
         LocationReadingsDto FetchLocationDetailMonth(string Province, string City, DateTime Date);
 
-        // interface for accessing FetchLocationDetailYear in LocationReadingsDto.
+        // interface for accessing FetchLocationDetailYear from LocationReadingsDto.
         LocationReadingsDto FetchLocationDetailYear(string Province, string ciCityty, DateTime Date);
     }
 }
--- WeatherStationApi/04 Interfaces/Services/IRawReadingsService.cs
+++ WeatherStationApi/04 Interfaces/Services/IRawReadingsService.cs
@@ -5,9 +5,16 @@ namespace WeatherStationApi._04_Interfaces.Services
 {
     public interface IRawReadingsService
     {
+        // interface for accessing FetchRawDayAStationReadings from RawReadingsDto.
         RawReadingsDto FetchRawDayAStationReadings(int StationId, DateTime Date);
+
+        // interface for accessing FetchRawWeekAStationReadings from RawReadingsDto.
         RawReadingsDto FetchRawWeekAStationReadings(int StationId, DateTime Date);
+
+        // interface for accessing FetchRawMonthAStationReadings from RawReadingsDto.
         RawReadingsDto FetchRawMonthAStationReadings(int StationId, DateTime Date);
+
+        // interface for accessing FetchRawYearAStationReadings from RawReadingsDto.
         RawReadingsDto FetchRawYearAStationReadings(int StationId, DateTime Date);
     }
 }
