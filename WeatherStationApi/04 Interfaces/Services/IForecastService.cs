3b14bab6284bc5acdfe0987192d3517686d16633
Eon VIljoen <28807995@student.g.nwu.ac.za>
2019-10-16 19:32:38 +0200

    some mistakes

--- WeatherStationApi/04 Interfaces/Services/IForecastService.cs
+++ WeatherStationApi/04 Interfaces/Services/IForecastService.cs
@@ -2,6 +2,7 @@ using System;
 
 namespace WeatherStationApi._04_Interfaces.Services
 {
+    // explicitly states methods for ForecastDto.
     public interface IForecastService
     {
         // interface to access ForecastDtos.
--- WeatherStationApi/04 Interfaces/Services/ILocationReadingsService.cs
+++ WeatherStationApi/04 Interfaces/Services/ILocationReadingsService.cs
@@ -3,18 +3,12 @@ using WeatherStationApi._03_Dtos;
 
 namespace WeatherStationApi._04_Interfaces.Services
 {
+    // explicitly states methods for RawReadingsDto.
     public interface ILocationReadingsService
     {
-        // interface for accessing FetchLocationDetailDay from LocationReadingsDto.
         LocationReadingsDto FetchLocationDetailDay(string Province, string City, DateTime Date);
-
-        // interface for accessing FetchLocationDetailWeek from LocationReadingsDto.
         LocationReadingsDto FetchLocationDetailWeek(string Province, string City, DateTime Date);
-
-        // interface for accessing FetchLocationDetailMonth from LocationReadingsDto.
         LocationReadingsDto FetchLocationDetailMonth(string Province, string City, DateTime Date);
-
-        // interface for accessing FetchLocationDetailYear from LocationReadingsDto.
         LocationReadingsDto FetchLocationDetailYear(string Province, string ciCityty, DateTime Date);
     }
 }
